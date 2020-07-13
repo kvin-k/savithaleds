4af969ed61429f3805eae5817f26f43f4d66d4ad
K Vinith Kumar <37012080+kvin-k@users.noreply.github.com>
2020-07-13 21:02:32 +0530

    Third commit

--- src/pages/ProductCard.js
+++ src/pages/ProductCard.js
@@ -2,28 +2,30 @@ import React from 'react';
 import {Link} from 'gatsby'
 
 const ProductCard = ({location}) => {
-  if( location.state===undefined){
-    location.state.productDetails.title="title"
-    location.state.productDetails.content="content"
-    location.state.productDetails.image="image"
-  }
-  return (
-  <>
-  <div class="flex justify-between">
-    <div className="text-xl font-semibold">{location.state.productDetails.title}</div><div><button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full">
-  <Link to="/">Close</Link>
-</button></div>
-</div>
-    <p className="mt-6">{location.state.productDetails.content}</p>
-    <div className="flex items-center mt-8">
-      <img
-        className="w-120 h-120 mr-4 "
-        src={location.state.productDetails.productImage}
-        alt={location.state.productDetails.title}
-      />
+  if( location.state!==undefined){
+    return (
+      <>
+      <div class="flex justify-between">
+        <div className="text-xl font-semibold">{location.state.productDetails.title}</div><div><button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full">
+      <Link to="/">Close</Link>
+    </button></div>
     </div>
-    </>
-  )
+        <p className="mt-6">{location.state.productDetails.content}</p>
+        <div className="flex items-center mt-8">
+          <img
+            className="w-120 h-120 mr-4 "
+            src={location.state.productDetails.productImage}
+            alt={location.state.productDetails.title}
+          />
+        </div>
+        </>
+      )
+  }else{
+    return(
+      <div>Error</div>
+    )
+  }
+  
   
 }
 
